6322f8a334985f0dd684277f2cb4f301b6c65b8c
Chima20-art <69147358+Chima20-art@users.noreply.github.com>
2023-04-04 18:35:47 +0200

    added frontend folder.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react'
+
 import './App.css';
 
+
 function App() {
+  const [meal,setMeal] = useState({})
+const API = 'http://localhost:3001/api/getRandomMeal';
+
+const fetchMeal = () => {
+  fetch(API).then((res) => res.json()).then((res) => {
+    console.log(res)
+    setMeal(res)
+  })
+}
+useEffect(()=>{
+  fetchMeal()
+},[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-full  flex justify-center	items-center text-center ">
+    
+      <div className='bg-red-100 w-fit px-[40px] py-[30px] my-auto rounded-xl  shadow-black shadow-slate-50	'>
+      <form action="/action_page.php">
+      <h2 className='text-xl font-bold underline'>Today's Meal</h2>
+      <h1 className='text-3xl font-bold capitalize' >{meal.name}</h1>
+      <p className="my-[30px]">Remplissez la formule pour envoyez votre commande:</p>
+  <label>First name:</label><br/>
+  <input type="text" id="fname" name="fname" className='border border-black'/><br/>
+  <label >Last name:</label><br/>
+  <input type="text" id="lname" name="lname"  className='border border-black'/><br/><br/>
+  <label >Numero de telephone:</label><br/>
+  <input type="text" id="lname" name="lname" className='border border-black' /><br/><br/>
+  <label >Zone de livraison:</label><br/>
+  <input type="text" id="lname" name="lname" className='border border-black' /><br/><br/>
+  <input type="submit" value="Envoyez" className='border border-black px-6 py-2 text-white bg-black rounded-md cursor-pointer'/>
+</form>
+      </div>
     </div>
   );
 }
